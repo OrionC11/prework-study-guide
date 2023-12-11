6412e1502bc47e4578f09137992273ca31a46f1f
Orion Cannon <orion.m.cannon@gmail.com>
2023-12-10 21:54:37 -0700

    Added JavaScript for statements to the HTML and added notes in the HTML

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,47 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-for(var x=0; x < topics.length; x++) {
-    console.log(topics[x])
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+function listTopics() {
+
+    for(var x=0; x < randomTopicngth; x++) {
+        console.log(randomTopic)
+    }
 }
 
-/*
-if (topic === 'HTML') {
+function selectTopic() {
 
-  console.log("Let's study HTML!");
+    if (randomTopic === 'HTML') {
 
-} else if (topic === 'CSS') {
+      console.log("Let's study HTML!");
 
-  console.log("Let's study CSS!");
+    } else if (randomTopic === 'CSS') {
 
-} else if (topic === 'Git') {
+      console.log("Let's study CSS!");
 
-  console.log("Let's study Git!");
+    } else if (randomTopic === 'Git') {
 
-} else if (topic === 'JavaScript') {
+      console.log("Let's study Git!");
 
-  console.log("Let's study JavaScript!");
+    } else if (randomTopic === 'JavaScript') {
 
-} else {
+      console.log("Let's study JavaScript!");
 
-  console.log('Please try again!');
+    } else {
 
-}
-*/
+      console.log('Please try again!');
 
-var shapes = ["triangle", "square", "pentagon", "circle"];
+    }
+  }
 
-console.log(shapes[0]);
+console.log('Here are the topics we learned through Prework:');
+
+listTopics();
+
+console.log('Which topic should we study first');
+
+selectTopic();
+
+/* var shapes = ["triangle", "square", "pentagon", "circle"];
+
+console.log(shapes[0]);  */
